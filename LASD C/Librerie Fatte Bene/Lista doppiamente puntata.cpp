b3bdc528dc07d023d986cddd492cbf5300faa860
GGGoat98 <92877774+GGGoat98@users.noreply.github.com>
2023-03-17 09:43:23 +0100

    Lista doppiamente puntata modificata

--- LASD C/Librerie Fatte Bene/Lista doppiamente puntata.cpp
+++ LASD C/Librerie Fatte Bene/Lista doppiamente puntata.cpp
@@ -9,10 +9,11 @@ struct el
 	struct el *next;
 };
 
-struct el *crea_lista(int n, struct el* testa, struct el* testa1);
+struct el *crea_lista(int n);
 struct el *inserisci(struct el*,int);
 void visualizza_lista(struct el *lista);
 struct el* inserisci_testa(struct el* L, int valore);
+void print_lista_dopp(struct el *p, int n);
 
 int main(){
 	struct el *lista;
@@ -24,7 +25,7 @@ int main(){
 	while(liste != 0){
 		printf("Specificare numero di elementi\n");
 		scanf("%d", &n);
-		lista=crea_lista(n, testa, testa); 
+		lista=crea_lista(n); 
 		/*printf("\nInserisci elemento da inserire in coda: ");
 		scanf("%d", &valore);
 		lista=inserisci(lista,valore);
@@ -33,28 +34,36 @@ int main(){
 		scanf("%d", &valore2);
 		inserisci_testa(lista, valore2);
 		visualizza_lista(lista);*/
+		print_lista_dopp(lista, n);
 		liste--;
 	}
 }
 
 
-//lista doppiamente puntata
-struct el *crea_lista(int n, struct el* testa, struct el* testa1) {
-	
-	if(n != 0){
-		
-		testa=(struct el *)malloc(sizeof(struct el));
-		printf("inserire primo elemento\n");
-		scanf("%d",&testa->inf);
-		testa->next = NULL; //da inserire per dare un next iniziale
-		testa->prev = testa1;
-		testa1 = testa;
-		
-		n = n - 1;
-		testa->next = crea_lista(n, testa->next, testa1);
-	}
-	
-	return(testa);
+/*Lista doppiamente puntata circolare*/
+struct el *crea_lista(int n) {
+    struct el *p, *punt;
+    int i;
+    if(n==0) {
+        p = NULL;
+    } else {
+        /* creazione primo elemento */
+        p = (struct el*)malloc(sizeof(struct el));
+        printf("\nInserisci il primo valore: ");
+        scanf("%d", &p->inf);
+        punt = p;
+        p->prev = NULL;
+        for(i=2; i<=n; i++) {
+            punt->next = (struct el *)malloc(sizeof(struct el));
+            punt->next->prev = punt;
+            punt = punt->next;
+            printf("\nInserisci il %d elemento: ", i);
+            scanf("%d", &punt->inf);
+        } // chiudo il for
+        punt->next = p; // lista circolare: l'ultimo elemento punta alla testa
+        p->prev = punt; // la testa punta all'ultimo elemento
+    } // chiudo l'if-else
+    return(p); // ritorno il puntatore alla testa
 } // chiudo la funzione
 
 
@@ -120,6 +129,19 @@ void visualizza_lista(struct el *p) {
 	printf("NULL\n\n"); 
 } 
 
+
+void print_lista_dopp(struct el *p, int n) {
+	printf("\nlista ---> ");
+	int i = 0 ;
+	while(i < n){
+		
+		printf("%d", p->inf); /* visualizza l'informazione */
+		printf(" ---> ");
+		p = p->next;
+		i++; /* scorre la lista di un elemento */
+	}
+	printf("NULL\n\n"); 
+} 
 
 //inserimento in testa
 struct el* inserisci_testa(struct el* L, int valore){
